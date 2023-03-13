9873bf6f1697511ac3e790f05867b6901ea26761
Hendrik Wendt <h.wendt@gmx.com>
2023-03-12 20:55:33 -0400

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={
+         r"/api/*": {"origins": "https://feathr-frontend.herokuapp.com/"}})
 
     register_blueprints(app)
     register_errors(app)
